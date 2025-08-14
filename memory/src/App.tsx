f231c9c5fa174b8ca44ecdd78bf322e8a67ff164
Olivia <166169277+oliviaalmseger@users.noreply.github.com>
2025-08-14 13:37:25 +0200

    feat: add components map + memoryApp file

--- memory/src/App.tsx
+++ memory/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import { MemoryApp } from './components/MemoryApp'
 
 function App() {
   
   return (
-<></>
+  <>
+  <MemoryApp />
+  </>
   )
 }
 
---
+++ memory/src/components/MemoryApp.tsx
@@ -0,0 +1,5 @@
+export const MemoryApp = () => {
+    return (
+        <>Syns detta?</>
+    )
+}
